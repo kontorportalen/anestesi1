0328c80d9b06c2f5d030fc83d3bc2058dd6bbbd8
nafweb1 <webmaster.nafweb@gmail.com>
2023-11-02 10:02:54 +0100

    Første versjon

---
+++ src/lib/components/FrontpageHero.svelte
@@ -0,0 +1,12 @@
+<div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Anestesi1</h1>
+        <p class="mb-5">Hei der! Her kommer hjemmesidene til Anestesi1, et firma som tilbyr anestesitjenester til offentlige og private institusjoner.
+            Kom tilbake om litt for å få vite mer!
+        </p>
+<!--        <button class="btn btn-primary">Get Started</button> -->
+      </div>
+    </div>
+  </div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import FrontpageHero from "../lib/components/FrontpageHero.svelte";
+
+</script>
+
+<svelte:head>
+    <title>Anestesi1 | Hjem</title>
+</svelte:head>
+
+<FrontpageHero />
